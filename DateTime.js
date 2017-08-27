5cac585552809aaf754966d3b3ae094c036d17c7
Shaii Ong <monsteronfire@gmail.com>
2017-08-28 00:22:05 +0800

    Add spec to test valid Date type and non-Date args

---
+++ DateTime.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var DateTime = (function () {
+  function createDateTime(date) {
+    return {
+      get offset() {
+        return date.getTime();
+      }
+    };
+  }
+
+  return function () {
+    return createDateTime(new Date());
+  };
+})();
--- test/spec/DateTimeSpec.js
+++ test/spec/DateTimeSpec.js
@@ -7,4 +7,26 @@ describe("DateTime", function () {
     expect(offset).not.toBeLessThan(lowerLimit);
     expect(offset).not.toBeGreaterThan(upperLimit);
   });
+
+  it("matches the passed in Date when called with one argument", function () {
+    var dates = [new Date(), new Date(0), new Date(864e13), new Date(-864e13)];
+
+    for (var i = 0; i < dates.length; i++) {
+      expect(DateTime(dates[i]).offset).toEqual(dates[i].getTime());
+    }
+  });
+
+  it("throws an error when called with a single non-Date argument", function () {
+    var nonDates = [0, NaN, Infinity, "", "not a date", null, /regex/, {}, []];
+    for (var i = 0; i < nonDates.length; i++) {
+      expect(DateTime.bind(null, nonDates[i])).toThrow();
+    }
+  });
+
+  it("returns a NaN offset when an invalid date is passed in", function () {
+    var invalidDates = [new Date(864e13 + 1), new Date(-1e99), new Date("xyz")];
+    for(var i = 0; i < invalidDates.length; i++) {
+      expect(isNaN(invalidDates[i]).offset).toBe(true);
+    }
+  });
 });
